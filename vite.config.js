24b5ec30bbd87a40c1532ffbc9999946c06ba377
Monknow <juanrodriguezcode@gmail.com>
2022-08-23 08:30:54 -0400

    changed vite.config base to work with github

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 const {defineConfig} = require("vite");
 
 module.exports = defineConfig({
+	base: "/apis-you-didnt-know-about/",
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				origin: resolve(__dirname, "origin/index.html"),
+			},
+		},
+	},
 	server: {
 		host: true,
 		https: true,
