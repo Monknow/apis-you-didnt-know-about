a431cb8ddca840e93cc9c1761f00d7a95b08fb43
Monknow <juanrodriguezcode@gmail.com>
2022-08-23 09:02:13 -0400

    fixed vite config

--- dist/assets/origin.91e60c19.js
+++
@@ -1,1 +0,0 @@
-import"./style.0c45c0da.js";const n=new BroadcastChannel("quote_channel"),o=document.querySelector("#quote");n.onmessage=({data:e})=>{o.innerHTML=e};
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import {defineConfig} from "vite";
 import {resolve} from "path";
 
 export default defineConfig({
-	base: "/pwa-features/",
+	base: "/apis-you-didnt-know-about/",
 	build: {
 		rollupOptions: {
 			input: {
